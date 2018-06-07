55ca5bc5afa316463fd3879850998447f2a402df
Daniel Ivanov <denatwrk@gmail.com>
2018-06-07 20:28:22 +0300

    Added viewset class param to standard endpoint registration function.

--- drf_shortcuts/urls.py
+++ drf_shortcuts/urls.py
@@ -3,7 +3,7 @@ from inflection import dasherize, underscore
 from drf_shortcuts.views import create_standard_viewset_class
 
 
-def register_standard_endpoint(router, model):
+def register_standard_endpoint(router, model, viewset_cls=None):
     endpoint_name = dasherize(underscore(model.__name__))
-    viewset = create_standard_viewset_class(model)
+    viewset = viewset_cls or create_standard_viewset_class(model)
     router.register(endpoint_name, viewset, base_name=viewset.serializer_class.DEFAULT_BASE_NAME)
