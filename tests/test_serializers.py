a3433f165d0859c09b7e8b44efcc9512d53214d6
Daniel Ivanov <denatwrk@gmail.com>
2018-11-15 09:33:28 +0300

    Completed spec for get_entity_pk. Made egg-info ignore more generic.

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from rest_framework.serializers import ModelSerializer
+from rest_framework.views import APIView
 
 from drf_shortcuts.serializers import get_entity_pk
 
@@ -8,3 +10,17 @@ class GetEntityPkTests(TestCase):
         with self.assertRaises(AttributeError) as excCtx:
             get_entity_pk(None)
         self.assertTrue('context' in str(excCtx.exception))
+
+    def test_returns_none_if_pk_is_missing(self):
+        serializer = ModelSerializer()
+        view = APIView()
+        view.kwargs = {}
+        serializer.context['view'] = view
+        self.assertIsNone(get_entity_pk(serializer))
+
+    def test_returns_pk_value_if_present(self):
+        serializer = ModelSerializer()
+        view = APIView()
+        view.kwargs = {'pk': 'foo'}
+        serializer.context['view'] = view
+        self.assertEqual('foo', get_entity_pk(serializer))
