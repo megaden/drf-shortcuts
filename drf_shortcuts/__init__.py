8eceafa187ba471ebffc3a3ae94ef5dccd6df24e
Daniel Ivanov <denatwrk@gmail.com>
2018-11-30 10:00:08 +0300

    More of docs boilerplate. Started to put together proper docstrings.

--- drf_shortcuts/__init__.py
+++ drf_shortcuts/__init__.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+"""DRF (Django REST Framework) shortcuts package.
+
+Common shortcuts for speeding up your development based on Django REST Framework (DRF).
+
+Modules:
+
+- #serializers: Base classes & helper functions for creating serializer types.
+
+- #urls: Now contains only 'register_standard_endpoint' helper function for registering Model against DRF Router.
+
+- #views: Helper functions for creating view & viewset types.
+"""
+
 __title__ = 'DRF Shortcuts'
 __version__ = '0.0.1'
 __author__ = 'Daniel Ivanov'
--- drf_shortcuts/urls.py
+++ drf_shortcuts/urls.py
@@ -1,9 +1,35 @@
+# -*- coding: utf-8 -*-
+"""urls module of DRF (Django REST Framework) shortcuts package.
+
+Functions:
+
+- #register_standard_endpoint: Exposes Django model via DRF router.
+
+See also:
+
+- #drf_shortcuts.views.create_standard_viewset_class
+"""
+
 from inflection import dasherize, underscore
 
 from drf_shortcuts.views import create_standard_viewset_class
 
 
 def register_standard_endpoint(router, model, viewset_cls=None):
+    """Exposes API endpoints for Django model via DRF router.
+
+    Parameters:
+
+    - router #rest_framework.routers.BaseRouter: The instance of DRF router to add viewset to.
+
+    - model #django.db.models.Model: The model class to expose.
+
+    - viewset_cls #rest_framework.viewsets.GenericViewSet: The viewset to expose.
+
+        Optional hence if omitted the 'standard' viewset class will be used.
+
+        See also #drf_shortcuts.views.create_standard_viewset_class.
+    """
     endpoint_name = dasherize(underscore(model.__name__))
     viewset = viewset_cls or create_standard_viewset_class(model)
     router.register(endpoint_name, viewset, base_name=viewset.serializer_class.DEFAULT_BASE_NAME)
